97db171c3b2582ac54cbfd48549ac23de7bab0fd
SudhanshuKumarShekhar <shekhar2119@gmail.com>
2023-07-15 22:57:53 +0530

    angular login work

--- clientUI/src/app/_services/account.service.ts
+++ clientUI/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,9 +9,29 @@ import { Injectable } from '@angular/core';
 export class AccountService {
 
   baseUrl = 'http://localhost:5029/api/';
+private currentUserSourse = new BehaviorSubject<User | null>(null);
+currentUser$ = this.currentUserSourse.asObservable();
+
   constructor( private http:HttpClient ) { }
 
   login(model:any){
-    return this.http.post(this.baseUrl+'account/login',model);
+    return this.http.post<User>(this.baseUrl+'account/login',model).pipe(
+      map((response:User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUserSourse.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user:User){
+    this.currentUserSourse.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSourse.next(null);
   }
 }
--- clientUI/src/app/nav/nav.component.ts
+++ clientUI/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -7,24 +9,26 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  loggedIn: boolean = false;
+  currentUser$: Observable<User |null> = of(null);
   model: any = {}
-  constructor( private accountService :AccountService) { }
+  constructor( public accountService :AccountService) { }
 
   ngOnInit(): void {
   }
+
   logIn(){
     this.accountService.login(this.model).subscribe({
       next: response =>{
         console.log(response);
-        this.loggedIn =true;
+
       },
       error: error => console.log(error)
     })
 
   }
   logout(){
-    this.loggedIn =false
+    this.accountService.logout();
+
   }
 
 }
